6af20ccab5143149f53aa3a36beebf73bcf2fcba
Nand Kishore <nandk4552@gmail.com>
2024-07-15 17:16:25 +0530

    final commit

---
+++ app/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI;
+import joblib
+import numpy as np
+
+model = joblib.load("app/model.joblib")
+class_names = np.array(["setosa", "versicolor", "virginica",])
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Iris model API"}
+
+@app.post("/predict")
+def predict(data: dict):
+    features = np.array(data["features"]).reshape(1, -1)
+    prediction = model.predict(features)
+    class_name = class_names[prediction][0]
+    return {"prediction_class": class_name}
